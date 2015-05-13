c234d67c0689b4df22350ab80e796daf271431a6
Connor Olding <cloningdonor@gmail.com>
2015-05-13 11:15:49 -0700

    more data

--- Lua/data/_ei.lua
+++ Lua/data/_ei.lua
@@ -1,6 +1,13 @@
 return {
 	[0] = true,
 	[2] = true,
+	[7] = true,
+	[8] = true,
+	[9] = true,
+	[10] = true,
+	[13] = true,
+	[14] = true,
+	[24] = true,
 	[40] = true,
 	[41] = true,
 	[42] = true,
