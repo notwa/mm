bc6037092ecf9ea9ccc689a3285a3833a40f72b8
Connor Olding <cloningdonor@gmail.com>
2015-12-26 23:22:15 -0800

    print to file

--- Lua/print.lua
+++ Lua/print.lua
@@ -1,3 +1,4 @@
+-- use with inject.lua on O EUDB MQ
 require = require "depend"
 require "boilerplate"
 
@@ -5,27 +6,29 @@ local buffer = 0x700070
 
 local vfc = A(0x168960, 4)
 
-while true do
+-- $ tail -f oot\ debug.txt | iconv -f euc-jp
+local f = io.open('oot debug.txt', 'w+b')
+
+while version == "O EUDB MQ" do
     local pos = buffer
     local str = ''
+    local fmt = '%c'
     while true do
         local b = R1(pos)
         pos = pos + 1
         if b == 0 then
             break
         end
-        if b < 0x80 then
-            str = str..string.char(b)
-        else
-            str = str..'?'
-        end
+        str = str..string.char(b)
     end
-    print(str)
+    f:write(str)
+    f:flush()
     local old = vfc()
     for i=1,30 do
         emu.frameadvance()
         local new = vfc()
         if new ~= old then break end
     end
-    console.clear() -- delete this if you want
 end
+
+f:close()
