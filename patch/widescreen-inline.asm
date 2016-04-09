f3f27f5676392638832555d1a6577e171cd68811
Connor Olding <cloningdonor@gmail.com>
2016-04-09 04:47:53 -0700

    add widescreen romhack

--- patch/widescreen-inline.asm
+++ patch/widescreen-inline.asm
@@ -6,48 +6,78 @@
 [ctxt]: 0x80212020
 [dlists]: 0x80168930
 
+[DMARomToRam]: 0x80000BFC
+
+[vstart]: 0x035D0000 
+[start]: 0x80700000
+[size]: 0x10000
+
+/*
+.org 0x18F30
+; add an entry to the end of dmatable to hold our extra code
+; this actually just crashes the game so don't bother
+; (no debug filename associated with it = bad pointer dereference? maybe?)
+    .word @vstart    ; virtual start
+    .word 0x035E0000 ; virtual end (@vstart + @size)
+    .word @vstart    ; physical start (should be same as virtual start)
+    .word 0          ; physical end (should be 0 for uncompressed)
+*/
+
+.org 0xB3D9E4 ; 0x800C6844
+    ; this appears to be the main game loop function
+    ; we can "make room" for some injected code
+    ; by taking advantage of it never returning under normal circumstances.
+    ; we'll cut out pushing RA, S1-S8 stuff on stack.
+    ; props to CloudMax for coming up with this.
+    addiu   sp, sp, 0xFC60 ; original code
+    ; push removed here
+    li      s0, 0x8011F830 ; original code
+    ; pushes removed here
+    ; 9 instructions to work with?
+    ; dma args are backwards compared to MM?
+    li      a1, @start
+    li      a2, @size
+    jal     @DMARomToRam
+    li      a0, @vstart
+    lui     a0 0x8014 ; original code
+    cl      a1
+    cl      a2
+    nop
+    nop
+    nop
+    nop
+    nop
+
+[original]: 0x800C6AC4
+[inject_from]: 0xB3D458 ; 0x800C62B8
+[inject_to]: 0x80700000
+
+.org @inject_from
+    jal     @inject_to
+
+.org @inject_to
+    sw      ra, -4(sp)
+    sw      a0,  0(sp)
+    sw      a1,  4(sp)
+    sw      a2,  8(sp)
+    sw      a3, 12(sp)
+    bal     start
+    subi    sp, sp, 24
+    lw      ra, 20(sp)
+    lw      a0, 24(sp)
+    lw      a1, 28(sp)
+    lw      a2, 32(sp)
+    lw      a3, 36(sp)
+    j       @original
+    addi    sp, sp, 24
+
+start:
     push    4, 1, s0, s1, s2, s3, s4, s5, ra
 
 ; time for dlist misuse
     li      t0, @dlists
     lw      t0, 0x04(t0) ; HUD display list start
     cl      t2 ; iteration count
-
-; DEBUG
-/*
-;; kill an entire display list by ending it immediately
-;    li      t9, 0xE9000000
-;    sw      t9, 0(t0)
-;    sw      r0, 4(t0)
-;    li      t9, 0xDF000000
-;    sw      t9, 8(t0)
-;    sw      r0, 0xC(t0)
-;    b       finish ; DEBUG
-;    nop
-
-; nop certain command types
-    cl      t2 ; iteration count
-    cl      t3 ; functions overwritten
-[nop_target]: 256
--:
-    bgei    t2, 0x1000, + ; give up after a while
-    addiu   t2, t2, 1
-    bgei    t3, @nop_target, + ; stop after this many commands nop'd
-    nop
-    lw      t1, (t0) ; load command
-    li      t9, 0xDF000000
-    beq     t1, t9, + ; stop if we're at the end of the display list
-    li      t9, 0xDE000000 ; check if it's a call
-;   li      t9, 0xDC080008 ; sets the video resolution and whatnot
-    bne     t1, t9, -
-    addiu   t0, t0, 8 ; next command
-    sw      r0, -8(t0) ; nop the last command
-    sw      r0, -4(t0)
-    b       -
-    addiu   t3, t3, 1
-+:
-    jpop    4, 1, s0, s1, s2, s3, s4, s5, ra
-*/
 
 -:
     bgei    t2, 0x1000, finish ; give up after a while
@@ -66,20 +96,6 @@
     nop
 
 texscale:
-; DEBUG
-/*
-; nop an entire 2d texture draw command sequence
-    sw      r0, -8(t0)
-    sw      r0, -4(t0)
-    sw      r0, 0(t0)
-    sw      r0, 4(t0)
-    sw      r0, 8(t0)
-    sw      r0, 0xC(t0)
-    addiu   t0, t0, 0x10 ; next 2 commands
-    b       -
-    nop
-*/
-
 ; get the coordinate and scale data
     andi    s1, t1, 0xFFF ; get bottom right y
     srl     t3, t1, 12
@@ -129,25 +145,16 @@ texscale:
     addiu   t0, t0, 0x10 ; next two commands
 
 screen_dim: ; handle screen dimensions for A Button, beating heart, map icons
-/* NOTES:
-1C4164
-heart/map matrix DA380007 801BEBA0
-a button matrix DA380007 801BF0D0 (thereabouts)
-search for DC080008
-a button sometimes: 1CFD60?
-0342007E 01FF0000
-DC080008 801CFD60
-*/
     lw      t1, 4(t0)
     lw      t4, 0(t1)
     li      t9, 0x005A005A ; probably the A button, we want it to be 0x0044
     beq     t4, t9, abutt
     li      t9, 0x028001E0 ; general screen stuff
+    ; FIXME: this causes the "jiggling" effect.
+    ;        we need to be more picky about which structs we modify.
     beq     t4, t9, general
     nop
     sw      t4, debug
-;    b       desperate
-;    nop
     b       next
     nop
 abutt:
@@ -161,10 +168,6 @@ general:
     li      t9, 0x01E001E0
     b       next
     sw      t9, 0(t1)
-;desperate:
-;    li      t9, 0x00200020
-;    b       next
-;    sw      t9, 0(t1)
 next:
     b       -
     addiu   t0, t0, 8 ; next command
@@ -210,9 +213,9 @@ scale_step:
 [res2_T]: 30
 [res2_R]: 320
 [res2_B]: 210
-.org 0x800AAB90
+.org 0xB21D30 ; 0x800AAB90
     li      t3, @res2_B ; 240B00D2
     li      t4, @res2_T ; 240C001E
-.org 0x800AABA8
+.org 0xB21D48 ; 0x800AABA8
     li      t1, @res2_R ; 24090140
     li      t2, @res2_L ; 240A0000
