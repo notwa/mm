9e39cf0f4cb4b167eea39da8f39bc3f664366b56
Connor Olding <cloningdonor@gmail.com>
2016-09-27 12:20:56 -0700

    update lua text monitor for previous commit

--- Lua/monitor debug text.lua
+++ Lua/monitor debug text.lua
@@ -1,13 +1,16 @@
 -- use with inject.lua on O EUDB MQ
 require "lib.setup"
 require "boilerplate"
+require "addrs.init"
 
-local buffer = 0x700180
+local buffer = 0x700700
 
 local vfc = A(0x168960, 4)
 
 -- $ tail -f oot\ debug.txt | iconv -f euc-jp
 local f = io.open('oot debug.txt', 'w+b')
+
+local ignore_vframes = true
 
 while version == "O EUDB MQ" do
     local pos = buffer
@@ -23,11 +26,21 @@ while version == "O EUDB MQ" do
     end
     f:write(str)
     f:flush()
-    local old = vfc()
-    for i=1,30 do
+    if ignore_vframes then
+        if R4(0x7006FC) == 0 then
+            W4(0x7006F8, 0x80700700)
+            W1(0x700700, 0)
+        else
+            print('buffer in use')
+        end
         emu.frameadvance()
-        local new = vfc()
-        if new ~= old then break end
+    else
+        local old = vfc()
+        for i=1,30 do
+            emu.frameadvance()
+            local new = vfc()
+            if new ~= old then break end
+        end
     end
 end
 
