43315e1ea391114fc31a218a56b4f4efc8f919a9
Connor Olding <cloningdonor@gmail.com>
2016-10-02 14:42:45 -0700

    add custom scene order for warp menu; adjust names

--- Lua/menus/warp.lua
+++ Lua/menus/warp.lua
@@ -8,27 +8,7 @@ end
 
 local per_page = 16
 
-local scenes_pages = {}
-for si=0x00,0x7F do
-    local i = scene_id_to_entrance_id[si]
-    local page = math.floor(si/per_page) + 1
-
-    if si % per_page == 0 then
-        scenes_pages[page] = {}
-        local s = ("Warp to Scene #%i/8"):format(page)
-        ins(scenes_pages[page], Text(s))
-    end
-
-    local entrance_items = {}
-    local entrances = {}
-    local scene_name = '[crash]'
-    if i ~= nil then
-        entrances = entrance_names[i]
-        scene_name = entrances.name
-    end
-
-    ins(entrance_items, Text( ("Warp to %s"):format(scene_name) ))
-
+local function fill_entrances(entrance_items, entrances, si)
     for j=0,32 do
         local ename = entrances[j]
         if ename == nil then
@@ -46,22 +26,83 @@ for si=0x00,0x7F do
     ins(entrance_items, Text("Cutscenes... (TODO)"))
     ins(entrance_items, Text(""))
     ins(entrance_items, Back())
-    local entrance_menu = Menu{Screen(entrance_items)}
-    ins(scenes_pages[page], LinkTo(scene_name, entrance_menu))
+end
 
-    if si % per_page == per_page - 1 then
-        ins(scenes_pages[page], Text(""))
-        ins(scenes_pages[page], Back())
+local scenes_pages = {}
+if entrance_names.custom_order then
+    local lut = {}
+    for si=0x00,0x7F do
+        local i = scene_id_to_entrance_id[si]
+        if i ~= nil then
+            local e = entrance_names[i]
+            if e.name ~= nil then
+                --print(e.name, ("%04X"):format(si * 0x200))
+                lut[e.name] = {si=si, i=i}
+            end
+        end
+    end
+
+    local page = 1
+    scenes_pages[page] = {}
+    for i, v in ipairs(entrance_names.custom_order) do
+        if v == "\n" then
+            page = page + 1
+            scenes_pages[page] = {}
+        elseif lut[v] ~= nil then
+            local entrance_items = {}
+            local entrances = entrance_names[lut[v].i]
+            local scene_name = entrances.name
+
+            ins(entrance_items, Text( ("Warp to %s"):format(scene_name) ))
+            fill_entrances(entrance_items, entrances, lut[v].si)
+            local entrance_menu = Menu{Screen(entrance_items)}
+            ins(scenes_pages[page], LinkTo(scene_name, entrance_menu))
+        else
+            ins(scenes_pages[page], Text(v))
+        end
+    end
+
+    for i=1,page do
+        local s = ("Warp to Scene #%i/%i"):format(i, page)
+        ins(scenes_pages[i], 1, Text(s))
+        ins(scenes_pages[i], Text(""))
+        ins(scenes_pages[i], Back())
+    end
+
+else
+    for si=0x00,0x7F do
+        local i = scene_id_to_entrance_id[si]
+        local page = math.floor(si/per_page) + 1
+
+        if si % per_page == 0 then
+            scenes_pages[page] = {}
+            local s = ("Warp to Scene #%i/8"):format(page)
+            ins(scenes_pages[page], Text(s))
+        end
+
+        local entrance_items = {}
+        local entrances = {}
+        local scene_name = '[crash]'
+        if i ~= nil then
+            entrances = entrance_names[i]
+            scene_name = entrances.name
+        end
+
+        ins(entrance_items, Text( ("Warp to %s"):format(scene_name) ))
+        fill_entrances(entrance_items, entrances, si)
+        local entrance_menu = Menu{Screen(entrance_items)}
+        ins(scenes_pages[page], LinkTo(scene_name, entrance_menu))
+
+        if si % per_page == per_page - 1 then
+            ins(scenes_pages[page], Text(""))
+            ins(scenes_pages[page], Back())
+        end
     end
 end
 
-return Menu{
-    Screen(scenes_pages[1]),
-    Screen(scenes_pages[2]),
-    Screen(scenes_pages[3]),
-    Screen(scenes_pages[4]),
-    Screen(scenes_pages[5]),
-    Screen(scenes_pages[6]),
-    Screen(scenes_pages[7]),
-    Screen(scenes_pages[8]),
-}
+local screens = {}
+for i, v in ipairs(scenes_pages) do
+    ins(screens, Screen(v))
+end
+
+return Menu(screens)
